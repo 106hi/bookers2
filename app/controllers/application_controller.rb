654ce5aca9f9b5222bb074e33c577cd48855117b
EC2 Default User <ec2-user@ip-172-31-8-37.ap-northeast-1.compute.internal>
2023-04-17 06:55:53 +0000

    [Add]Sign in,log inページ名前保存、名前ログインできるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-      users_path
+      user_path(current_user.id)
     end
 
     def after_sign_out_path_for(resource)
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
